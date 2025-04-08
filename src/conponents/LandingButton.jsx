1231c01fdb2b1eadd75b0436dfb8ee37406f9da7
chronialsgame <chronialsgame@gmail.com>
2025-04-08 21:44:00 +0530

    Update LandingButton.jsx

--- src/conponents/LandingButton.jsx
+++ src/conponents/LandingButton.jsx
@@ -13,7 +13,7 @@ export const LandingButton = () =>{
         <div className="h-[66%]"></div>
         <button type="button"
           onClick={() => {navigate('/home')}}
-          className="pointer-events-auto text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition duration-50"
+          className="pointer-events-auto text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition duration-500"
         >
           ENTER
         </button>
